0f1cbcf632270859e33b3f142412dfbd309da950
Deep Shah <deep@makkajai.com>
2017-01-27 18:08:13 +0530

    Treating the weighted mesh same as the linked mesh <Deep>

--- cocos/editor-support/spine/SkeletonJson.c
+++ cocos/editor-support/spine/SkeletonJson.c
@@ -853,7 +853,7 @@ spSkeletonData* spSkeletonJson_readSkeletonData (spSkeletonJson* self, const cha
 						type = SP_ATTACHMENT_REGION;
 					else if (strcmp(typeString, "mesh") == 0)
 						type = SP_ATTACHMENT_MESH;
-					else if (strcmp(typeString, "linkedmesh") == 0)
+					else if (strcmp(typeString, "linkedmesh") == 0 || strcmp(typeString, "weightedmesh") == 0 || strcmp(typeString, "skinnedmesh") == 0)
 						type = SP_ATTACHMENT_LINKED_MESH;
 					else if (strcmp(typeString, "boundingbox") == 0)
 						type = SP_ATTACHMENT_BOUNDING_BOX;
