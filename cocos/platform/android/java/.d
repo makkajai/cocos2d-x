0078421b87ab4e7f39c3d36a51ee04eb587c4f1e
Deep Shah <deep@makkajai.com>
2017-06-05 15:06:17 +0530

    Pre-building the cocos2dx libraries for Android <Deep>

---
+++ cocos/platform/android/java/.d
@@ -0,0 +1,4 @@
+/Users/batman/playground/projarea/numbersense/cocos2d/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java : \
+  /Users/batman/playground/projarea/numbersense/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl
+
+/Users/batman/playground/projarea/numbersense/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl :
